bbae2e9dd272e18bde34126a613ccf9755d4ba53
FNU-Tripti <tfnu@hawk.iit.edu>
2022-04-05 20:32:18 -0500

    Added the Javascript function
    - Added the Eveny Listner Function
    - Implemented both the functionalities.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-    //add your javascrip between these two lines of code
-     
-    
-    
-    
-    
-    
-    
-    
-    window.addEventListener('load', init);
+  //add your javascrip between these two lines of code
+  function listnerFunction(){
+    var entryInputValue = document.getElementById("entryinput").value;
+    alert("FNU Tripti: "+ entryInputValue);
+    var textboxRef =  document.getElementById("textoutput");
+    textboxRef.innerHTML = entryInputValue;
+  }
+  // adding the click event listener on the click of entrybutton div
+  var listenerReference = document.getElementById("entrybutton");
+  listenerReference.addEventListener('click', listnerFunction); 
+}   
+        
+window.addEventListener('load', init);
